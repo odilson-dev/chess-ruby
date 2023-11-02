234a1dc74ebc406e90d73ed39d17d57f6cf0ea8c
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-11-02 00:17:46 -0400

    Implement #find_allowed_moves for King's class

--- lib/pieces/king.rb
+++ lib/pieces/king.rb
@@ -4,8 +4,24 @@ class King < Piece
     def initialize(position, color, symbol)
         super(position, color, symbol)
     end
+
+    # This method allows the this piece to find all his allowed moves from any position on the table
+    def find_allowed_moves(x = @position[0], y = @position[1])
+        allowed_moves = []
+        # for x
+        for_x = [[x, y + 1], [x, y - 1]]
+
+        # for_x - 1
+        for_x_minus_1 = [[x - 1, y + 1], [x - 1, y], [x - 1, y - 1]]
+
+        # for_x + 1
+        for_x_plus_1 = [[x + 1, y + 1], [x + 1, y], [x + 1, y - 1]]
+
+        allowed_moves.concat(for_x, for_x_minus_1, for_x_plus_1)
+        allowed_moves.select { |position| (position[0] >= 1 && position[0] <= 8) && (position[1] >= 1 && position[1] <= 8) }
+    end
 end
 
 king = King.new([5, 5], "White", "H")
 
-p king.captured
+p king.find_allowed_moves
---
+++ spec/lib/pieces/king_spec.rb
@@ -0,0 +1,34 @@
+require_relative '../../../lib/pieces/king.rb'
+
+RSpec.describe King do
+    RSpec::Matchers.define :be_same_as do |second_array|
+        match do |first_array|
+          first_array.length == second_array.length ? first_array.map { |position| second_array.include? position }.all? : false
+        end
+      end
+    describe "#find_allowed_moves" do
+        context "When the position of the king is in the middle of the chessboard (eg: [5, 5])" do
+            let(:king) { King.new([5, 5], "White", "H") }
+            moves_allowed = [[5, 6], [5, 4], [4, 6], [4, 5], [4, 4], [6, 6], [6, 5], [6, 4]]
+            it "The allowed moves are: [[5, 6], [5, 4], [4, 6], [4, 5], [4, 4], [6, 6], [6, 5], [6, 4]]" do
+                expect(king.find_allowed_moves).to be_same_as moves_allowed
+            end
+        end
+
+        context "When the position of the king is at the bottom of the chessboard(eg: [4, 1])" do
+            let(:king) { King.new([4, 1], "White", "H") }
+            moves_allowed = [[3,1], [3, 2], [4, 2], [5,2], [5, 1]]
+            it "The allowed moves are: [[3,1], [3, 2], [4, 2], [5,2], [5, 1]]" do
+                expect(king.find_allowed_moves).to be_same_as moves_allowed
+            end
+        end
+
+        context "When the position of the king is at the corner of the chessboard (eg: [8, 8])" do
+            let(:king) { King.new([8, 8], "White", "H") }
+            moves_allowed = [[7, 8], [7, 7], [8, 7]]
+            it "The allowed moves are: [[7, 8], [7, 7], [8, 7]]" do
+                expect(king.find_allowed_moves).to be_same_as moves_allowed
+            end
+        end
+    end
+end
