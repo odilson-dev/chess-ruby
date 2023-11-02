aa7807ef19f0aca47d3bb8f0f736f5b4b71b2e8e
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-11-01 22:11:12 -0400

    Add test for #find_alllowed_moves method

--- lib/pieces/piece.rb
+++ lib/pieces/piece.rb
@@ -1,4 +1,5 @@
 class Piece
+    attr_accessor :position, :color, :symbol, :captured, :allowed_moves
     def initialize(position, color, symbol)
         @position = position
         @color = color
