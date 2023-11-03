a190e43e705aaa6d55bb26201b284ded59a16347
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-11-02 21:53:13 -0400

    Implement the method ChessBoard#add
    This method adds the piece given in parameter to his exact position
    on the chessboard

--- lib/chessboard.rb
+++ lib/chessboard.rb
@@ -1,13 +1,28 @@
 require_relative 'displayable'
+require_relative 'pieces/king.rb'
 
 
 class ChessBoard
     include Displayable
-    attr_accessor :data
+    attr_accessor :data, :active_square
     def initialize
-        @data = Array.new(8, Array.new(8))
+        @data = Array.new(8) { Array.new(8) }
+        @active_square = nil
     end
-end
 
-board_test = ChessBoard.new
-board_test.display_chess_board
+    def add(this_piece)
+        x = this_piece.position[1]
+        y = this_piece.position[0]
+        @data[y][x] = this_piece
+    end
+end 
+# king = King.new([5, 5], "white")
+
+
+# board_test = ChessBoard.new
+
+# board_test.add(king)
+# board_test.active_square = king
+
+
+# board_test.display_chess_board
--- spec/lib/chessboard_spec.rb
+++ spec/lib/chessboard_spec.rb
@@ -5,16 +5,23 @@ RSpec.describe ChessBoard do
     subject {chessboard_1 = ChessBoard.new}
     describe "#add" do
         king = King.new([3, 7], "white")
+
+        context "The method #add hasn't called yet all the squares on the chessboard are empty" do
+            it "The square a the position [3, 7] is nil" do
+                expect(subject.data[3][7]).to be_nil
+            end
+        end
+
         context "The method #add adds a piece on the chessboard by giving the piece instance as parameter" do
             before do
                 subject.add(king)
             end
             it "Adds the piece king at the position [3, 7] on the board" do
-                expect(subject[3][7]).to be king
+                expect(subject.data[3][7]).to be king
             end
 
             it "The position [3, 7] on the chessboard is not nil" do
-                expect(subject[3][7]).to_not be_nil
+                expect(subject.data[3][7]).to_not be_nil
             end
         end
     end
