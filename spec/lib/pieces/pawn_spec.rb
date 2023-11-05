dac2d168ee697e996a82ee8f2b62431ac3f1c3f3
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-11-04 20:34:26 -0400

    Add tests for Pawn#find_allowed_moves method

---
+++ spec/lib/pieces/pawn_spec.rb
@@ -0,0 +1,42 @@
+require_relative '../../../lib/pieces/pawn.rb'
+
+RSpec.describe Pawn do
+    RSpec::Matchers.define :be_same_as do |second_array|
+        match do |first_array|
+          first_array.length == second_array.length ? first_array.map { |position| second_array.include? position }.all? : false
+        end
+      end
+    describe "#find_allowed_moves" do
+        context "When the position of the white pawn is in the middle of the chessboard (eg: [4, 3])" do
+            let(:pawn) { Pawn.new([4, 3], "White", "H") }
+            moves_allowed = [[3, 3]]
+            it "The allowed moves are: [[[3, 3]]" do
+                expect(pawn.find_allowed_moves).to be_same_as moves_allowed
+            end
+        end
+
+        context "When the position of the white pawn is on the second line, point of view of the owner (eg: [6, 5])" do
+            let(:pawn) { Pawn.new([6, 5], "White", "H") }
+            moves_allowed = [[5, 5], [4, 5]]
+            it "The allowed moves are: [[5, 5], [4, 5]]" do
+                expect(pawn.find_allowed_moves).to be_same_as moves_allowed
+            end
+        end
+
+        context "When the position of the black pawn is in the middle of the chessboard (eg: [4, 3])" do
+            let(:pawn) { Pawn.new([4, 3], "Black", "H") }
+            moves_allowed = [[5, 3]]
+            it "The allowed moves are: [[5, 3]]" do
+                expect(pawn.find_allowed_moves).to be_same_as moves_allowed
+            end
+        end
+
+        context "When the position of the black pawn is on the second line, point of view of the owner (eg: [1, 4])" do
+            let(:pawn) { Pawn.new([1, 4], "Black", "H") }
+            moves_allowed = [[2, 4], [3, 4]]
+            it "The allowed moves are: [[2, 4], [3, 4]]" do
+                expect(pawn.find_allowed_moves).to be_same_as moves_allowed
+            end
+        end
+    end
+end
