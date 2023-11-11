6a00d416eb4f41970d8b38ebb19ae36dd37e9e15
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-11-11 12:53:13 -0500

    Implement #convert method

--- lib/game.rb
+++ lib/game.rb
@@ -8,47 +8,72 @@ class ChessGame
         @chessboard = ChessBoard.new
         
     end
-    def play_with_two_humans
-        puts "Player-1 What is your name?"
-        name1 = gets.chomp
-        player_1 = Player.new(name1, "white")
+    # def play_with_two_humans
+    #     puts "Player-1 What is your name?"
+    #     name1 = gets.chomp
+    #     player_1 = Player.new(name1, "white")
 
-        puts "Okay, #{player_1.name}, the white pieces have been assigned to you"
+    #     puts "Okay, #{player_1.name}, the white pieces have been assigned to you"
 
-        puts "Player-2 What is your name?"
-        name2 = gets.chomp
-        player_2 = Player.new(name2, "black")
+    #     puts "Player-2 What is your name?"
+    #     name2 = gets.chomp
+    #     player_2 = Player.new(name2, "black")
 
-        puts "Okay, #{player_2.name}, the black pieces have been assigned to you"
-        @chessboard.display_chess_board
+    #     puts "Okay, #{player_2.name}, the black pieces have been assigned to you"
+    #     @chessboard.display_chess_board
 
-        loop do
-            puts "It's #{player_1.name}'s turn"
-            puts "Choose the piece you want to move. eg(d1)"
-            position_of_the_choosed_piece = gets.chomp
-            if position_of_the_choosed_piece.length != 2
-                puts "Invalid input".light_red
-                puts "Your position should be two characters length".light_blue
-                redo
+    #     loop do
+    #         puts "It's #{player_1.name}'s turn"
+    #         puts "Choose the piece you want to move. eg(d1)"
+    #         position_of_the_choosed_piece = gets.chomp
+    #         if position_of_the_choosed_piece.length != 2
+    #             puts "Invalid input".light_red
+    #             puts "Your position should be two characters length".light_blue
+    #             redo
                 
-            elsif (("a".."h").to_a.include? position_of_the_choosed_piece[0] && ("1".."8").to_a.include? position_of_the_choosed_piece[1])
-                @chessboard.active_piece = 
+    #         elsif (("a".."h").to_a.include? position_of_the_choosed_piece[0] && ("1".."8").to_a.include? position_of_the_choosed_piece[1])
+    #             @chessboard.active_piece = 
                 
-            else
-                puts "Invalid input".light_red
-                puts "Your position should be composed of a letter between 'a' and 'h' and a digit between '1' and '8'".light_blue
-                redo
+    #         else
+    #             puts "Invalid input".light_red
+    #             puts "Your position should be composed of a letter between 'a' and 'h' and a digit between '1' and '8'".light_blue
+    #             redo
                 
-            end
-        end
-    end
+    #         end
+    #     end
+    # end
 
     # convert player inputs to arrays locgic
     def convert(input)
+        input_array = input.split("")
+        dict_alpha = {
+            :a => 0,
+            :b => 1,
+            :c => 2,
+            :d => 3,
+            :e => 4,
+            :f => 5,
+            :g => 6,
+            :h => 7
+        }
+        dict_digit = {
+            1 => 7,
+            2 => 6,
+            3 => 5,
+            4 => 4,
+            5 => 3,
+            6 => 2,
+            7 => 1,
+            8 => 0
+        }
+        return [dict_digit[input_array[1].to_i], dict_alpha[input_array[0].to_sym]]
         
     end
 
 end
+
+game = ChessGame.new
+p game.convert("f5")
 
 # chessboard = ChessBoard.new
 
