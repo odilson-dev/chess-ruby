fe11fbc3dfa8bbaa28ebc6dc5844b9eed9da7ec2
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-11-14 09:36:48 -0500

    Implement method choose_a_piece_to_move

--- lib/ai_computer_player.rb
+++ lib/ai_computer_player.rb
@@ -7,8 +7,27 @@ class AI < Player
         super(name, piece_color)
     end
 
+    def choose_a_piece_to_move(chessboard)
+        my_pieces = chessboard.data.flatten.select { |piece| piece&.color == piece_color }
+
+        pieces_with_attack_moves = my_pieces.select { |piece| piece.attack_moves.length > 0 }
+
+        pieces_with_allowed_moves = my_pieces.select { |piece| piece.allowed_moves.length > 0 }
+
+        if pieces_with_attack_moves.empty?
+
+            piece_choosed = pieces_with_allowed_moves.sample
+        else
+
+            piece_choosed = pieces_with_attack_moves.sample
+        end
+        
+        piece_choosed
+    end
 end
 
 chessboard = ChessBoard.new
 setup(chessboard)
 alexa = AI.new("Alexa", "white")
+
+p alexa.choose_a_piece_to_move(chessboard)
